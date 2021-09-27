65776f0005549f5874299780dc07da7acc779ee9
vkocjancic <kocjancic@hotmail.com>
2021-09-27 23:27:54 +0200

    Added main anvigation styling

--- TimeLogger_v2.App/scripts/layout.js
+++ TimeLogger_v2.App/scripts/layout.js
@@ -3,8 +3,26 @@
  * * * * * * * * * * * * * * * * * * */
 let templateLayoutDefault =
     '<div class="layout">' +
-    '    <header>Header</header>' +
-    '    <aside>Sidebar</aside>' +
+    '    <header class="header__main">' +
+    '         Header' +
+    '    </header> ' +
+    '    <aside class="sidebar">' +
+    '        <nav class="sidebar__nav--main">' +
+    '            <ul>' +
+    '                <li><a href="#" class="icon page__daily-logs">Daily logs</a></li>' +
+    '                <li><a href="#" class="icon page__insights">Insights</a></li>' +
+    '                <li><a href="#" class="icon page__tasks">Tasks</a></li>' +
+    '            </ul>' +
+    '        </nav>' +
+    '        <nav class="sidebar__nav--projects">' +
+    '            <h3>Projects</h3>' +
+    '            <ul>' +
+    '                <li><a href="#" class="icon page__project">Adacta</a></li>' +
+    '                <li><a href="#" class="icon page__project">TimeLogger</a></li>' +
+    '                <li><a href="#" class="icon page__project--add">Add project</a></li>' +
+    '            </ul>' +
+    '        </nav>' +
+    '    </aside>' +
     '    <main><slot /></main>' +
     '</div>';
 
