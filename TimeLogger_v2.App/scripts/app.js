df7de2a8a50a28d001177f817476424aa3474efa
vkocjancic <kocjancic@hotmail.com>
2022-02-24 21:01:32 +0100

    Page title includes page name

--- TimeLogger_v2.App/scripts/app.js
+++ TimeLogger_v2.App/scripts/app.js
@@ -5,20 +5,22 @@ import { HomeComponent, InsightsComponent, TasksComponent, NotYetImplementedComp
 const PrivacyPolicyComponent = { template: '<p>Privacy policy</p>' };
 const TermsOfServiceComponent = { template: '<p>Terms of service</p>' };
 
+const DEFAULT_TITLE = "TimeLoggerLive v2";
+
 const routes = [
     // public access routes
-    { name: 'CreateAccount', path: '/create', component: CreateAccountComponent, meta: { isPublicPage: true } },
-    { name: 'ForgotPassword', path: '/forgot', component: ForgotPasswordComponent, meta: { isPublicPage: true } },
-    { name: 'Login', path: '/login', component: LoginComponent, meta: { isPublicPage: true } },
+    { name: 'CreateAccount', path: '/create', component: CreateAccountComponent, meta: { isPublicPage: true, title: 'Create account' } },
+    { name: 'ForgotPassword', path: '/forgot', component: ForgotPasswordComponent, meta: { isPublicPage: true, title: 'Forgot password' } },
+    { name: 'Login', path: '/login', component: LoginComponent, meta: { isPublicPage: true, title: 'Login' } },
     { name: 'Logout', path: '/logout', component: LogoutComponent, meta: { isPublicPage: true } },
-    { name: 'PasswordReset', path: '/password-reset/:id', component: PasswordResetComponent, meta: { isPublicPage: true } },
-    { name: 'PrivacyPolicy', path: '/privacy', component: PrivacyPolicyComponent, meta: { isPublicPage: true } },
-    { name: 'TermsOfService', path: '/terms-of-service', component: TermsOfServiceComponent, meta: { isPublicPage: true } },
+    { name: 'PasswordReset', path: '/password-reset/:id', component: PasswordResetComponent, meta: { isPublicPage: true, title: 'Password reset' } },
+    { name: 'PrivacyPolicy', path: '/privacy', component: PrivacyPolicyComponent, meta: { isPublicPage: true, title: 'Privacy policy' } },
+    { name: 'TermsOfService', path: '/terms-of-service', component: TermsOfServiceComponent, meta: { isPublicPage: true, title: 'Terms of service' } },
     // restricted access routes
-    { name: 'Home', path: '/home', component: HomeComponent },
-    { name: 'Insights', path: '/insights', component: InsightsComponent },
-    { name: 'Tasks', path: '/tasks', component: TasksComponent },
-    { name: 'NotYetImplemented', path: '/nyi', component: NotYetImplementedComponent },
+    { name: 'Home', path: '/home', component: HomeComponent, meta: { title: 'Daily logs' } },
+    { name: 'Insights', path: '/insights', component: InsightsComponent, meta: { title: 'Insights' } },
+    { name: 'Tasks', path: '/tasks', component: TasksComponent, meta: { title: 'Tasks' } },
+    { name: 'NotYetImplemented', path: '/nyi', component: NotYetImplementedComponent, meta: { title: 'Not yet implemented' } },
     { path: '/', redirect: { name: 'Home' } }
 ];
 
@@ -38,7 +40,17 @@ router.beforeEach((to, from, next) => {
             next({ name: 'Login', query: { redirect: to.fullPath } });
         }
     }
-})
+});
+
+router.afterEach((to, from) => {
+    Vue.nextTick(() => {
+        let title = DEFAULT_TITLE;
+        if (to.meta.title) {
+            title += ' | ' + to.meta.title;
+        }
+        document.title = title;
+    });
+});
 
 const vue_app = Vue.createApp({});
 vue_app.use(router);
